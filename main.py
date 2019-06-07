573bafcecdb6ff68f81233634f5a5114b83c8629
ZAHN Hugo <hugo.zahn@soprasteria.com>
2019-06-07 11:22:30 +0200

    DEBUGING

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from TwitterInjector import TwitterInjector
+from confluent_kafka import Producer
 
 if __name__ == "__main__":
-
+    producer = Producer({'bootstrap.servers': 'localhost:9092'})
+    producer.produce("TutorialTopic", "FUCK YOU")
+    """
     print("==========STARTING===========")
     connector = TwitterInjector()
     connector.start_stream(follow="85741735")
     print("==========ENDING===========")
+    """
